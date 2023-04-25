f82b5dc340e5a54f200e692692a1bf24943a66fb
Mate Hartstein <67425173+mhartstein1@users.noreply.github.com>
2023-04-25 15:34:41 +0200

    update text printed

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 export PYTHONPATH=./
 echo "Installing requirements"
 pip install -r requirements.txt
-echo "Running SYNTHETIC experiment"
+echo "Starting Synthetic experiment"
 python src/experiment_scripts/synthetic_experiments.py
-echo "Running MNIST experiment"
+echo "Starting MNIST experiment"
 python src/experiment_scripts/mnist_experiments.py
-echo "Running SWARM experiment"
+echo "Starting Swarm experiment"
 python src/experiment_scripts/swarm_experiments.py
 echo "Generating Plots"
 python src/make_plots.py
